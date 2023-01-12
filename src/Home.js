a41eed87c2c0e15184259c3a1d074687afe3d64e
bestruirui <1004279909@qq.com>
2023-01-12 14:38:11 +0000

    init

--- src/Home.js
+++ src/Home.js
@@ -26,25 +26,17 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
-						<GitHubIcon />
-					</IconButton>
-				</div>
-				
 				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
+					<h1 style={{ fontSize: "45px" }}>WebRTC Meeting</h1>
+					<p style={{ fontWeight: "200" }}>基于WebRTC的视频会议</p>
 				</div>
-
 				<div style={{
 					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
 				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>加入一个会议</p>
 					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>加入</Button>
 				</div>
 			</div>
 		)
